307510db42e70a3e3dc0510184baa8d3367958be
Silased <129014832+Silased@users.noreply.github.com>
2025-05-07 01:20:12 -1000

    GUI Beautification/Improvements
    Reworked GUI to be more dynamic and utilize WPF dynamic capabilities better. Added GroupBoxes for aesthetic. Moved visibility/hiding functionality of the GUI elements away from Powershell and instead be directly within the XML via BooleanVisibilityConverter and bindings.

--- USAJobsGUI.ps1
+++ USAJobsGUI.ps1
@@ -4,62 +4,140 @@ Add-Type -AssemblyName presentationframework, presentationcore
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 
 ## Define USAJobs API Search Entry Points
-## $USAJobsSearchAPI= "https://data.usajobs.gov/api/search?"
-$USAJobsHistoricSearchAPI= "https://data.usajobs.gov/api/historicjoa?"
+$USAJobsSearchAPI = "https://data.usajobs.gov/api/search?"
+$USAJobsHistoricSearchAPI = "https://data.usajobs.gov/api/historicjoa?"
 
 $inputXML = @"
-    <Window x:Name="USAJobsAPISearch" x:Class="System.Windows.Window"
+<Window x:Class="System.Windows.Window"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
-        mc:Ignorable="d"
-        Title="USA Jobs API Searcher" Height="480" Width="550" Background="#FF313338">
-    <Grid>
-        <Label x:Name="TitleHeader" Content="USAJobs API Search" HorizontalAlignment="Center" VerticalAlignment="Top" Height="59" Width="320" FontSize="28" FontFamily="Cascadia Mono" Foreground="#FFF2F3F5" Margin="0,10,0,0"/>
-        <Label x:Name="LocationLabel" Content="Location:" Margin="66,229,0,0" Foreground="#FFF2F3F5" FontSize="16" HorizontalAlignment="Left" VerticalAlignment="Top"/>
-        <TextBox x:Name="LocationTextBox" HorizontalAlignment="Left" Margin="141,237,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="200" Height="20" Background="#FFF2F3E9" FontSize="14"></TextBox>
-        <TextBlock x:Name="LocationHint" IsHitTestVisible="False" Text="Ex: Japan, California, Europe" VerticalAlignment="Top" HorizontalAlignment="Left" Margin="144,239,0,0" Foreground="DarkGray" Width="156">
-            <TextBlock.Style>
-                <Style TargetType="{x:Type TextBlock}">
-                    <Setter Property="Visibility" Value="Collapsed"/>
-                    <Style.Triggers>
-                        <DataTrigger Binding="{Binding Text, ElementName=LocationTextBox}" Value="">
-                            <Setter Property="Visibility" Value="Visible"/>
-                        </DataTrigger>
-                    </Style.Triggers>
-                </Style>
-            </TextBlock.Style>
-        </TextBlock>
-        <Label x:Name="JobSeriesLabel" Content="Job Series:" Margin="55,263,0,0" Foreground="#FFF2F3F5" FontSize="16" HorizontalAlignment="Left" VerticalAlignment="Top"/>
-        <TextBox x:Name="JobSeriesTextBox" HorizontalAlignment="Left" Margin="141,271,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="200" Height="20" Background="#FFF2F3E9" FontSize="14"/>
-        <TextBlock x:Name="LocationHint_Copy" IsHitTestVisible="False" Text="Ex: 2210" VerticalAlignment="Top" HorizontalAlignment="Left" Margin="144,273,0,0" Foreground="DarkGray" Width="156">
-            <TextBlock.Style>
-                <Style TargetType="{x:Type TextBlock}">
-                    <Setter Property="Visibility" Value="Collapsed"/>
-                    <Style.Triggers>
-                        <DataTrigger Binding="{Binding Text, ElementName=JobSeriesTextBox}" Value="">
-                            <Setter Property="Visibility" Value="Visible"/>
-                        </DataTrigger>
-                    </Style.Triggers>
-                </Style>
-            </TextBlock.Style>
-        </TextBlock>
-        <Label x:Name="EmailAddressLabel" Content="API Email Address:" Margin="5,105,0,0" Foreground="#FFF2F3F5" FontSize="16" HorizontalAlignment="Left" VerticalAlignment="Top"/>
-        <Label x:Name="APIKeyLabel" Content="API Key:" Margin="80,141,0,0" Foreground="#FFF2F3F5" FontSize="16" RenderTransformOrigin="0.625,0.609" HorizontalAlignment="Left" VerticalAlignment="Top"/>
-        <TextBox x:Name="EmailAddressTextBox" HorizontalAlignment="Left" Margin="148,111,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="200" Height="20" Background="#FFF2F3E9" FontSize="14"/>
-        <TextBox x:Name="APIKeyTextBox" HorizontalAlignment="Left" Margin="148,148,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="370" Height="20" Background="#FFF2F3E9" FontSize="14"/>
-        <Label x:Name="APISectionLabel" Content="API Credentials" HorizontalAlignment="Left" Margin="148,69,0,0" VerticalAlignment="Top" Background="#FF313338" Foreground="#FFF2F3E6" FontSize="18"/>
-        <Label x:Name="SearchTermsLabel" Content="Search Criteria" HorizontalAlignment="Left" Margin="148,175,0,0" VerticalAlignment="Top" Background="#FF313338" Foreground="#FFF2F3E6" FontSize="18"/>
-        <CheckBox x:Name="RemoteJobCheckBox" Content="?Remote" Margin="71,206,0,0" VerticalAlignment="Top" Height="20" Width="84" FlowDirection="RightToLeft" Foreground="#FFF2F3F5" FontSize="16" HorizontalAlignment="Left"/>
-        <CheckBox x:Name="HistoricalCheckBox" Content="?Historical" Margin="239,206,0,0" VerticalAlignment="Top" Height="20" Width="102" FlowDirection="RightToLeft" Foreground="#FFF2F3F5" FontSize="16" HorizontalAlignment="Left"/>
-        <DatePicker x:Name="FromDatePicker" HorizontalAlignment="Left" Margin="141,306,0,0" VerticalAlignment="Top" FlowDirection="LeftToRight" Width="200"/>
-        <Label x:Name="FromDateLabel" Content="From Date:" Margin="52,302,0,0" Foreground="#FFF2F3F5" FontSize="16" HorizontalAlignment="Left" VerticalAlignment="Top"/>
-        <DatePicker x:Name="ToDatePicker" HorizontalAlignment="Left" Margin="140,342,0,0" VerticalAlignment="Top" FlowDirection="LeftToRight" Width="200"/>
-        <Label x:Name="ToDateLabel" Content="To Date:" Margin="72,337,0,0" Foreground="#FFF2F3F5" FontSize="16" HorizontalAlignment="Left" VerticalAlignment="Top"/>
-        <Button x:Name="SearchButton" Content="Search" HorizontalAlignment="Left" Margin="168,395,0,0" VerticalAlignment="Top" Height="38" Width="90"/>
-        <TextBlock x:Name="HistoricalWarningTextBlock" HorizontalAlignment="Left" Margin="273,395,0,0" TextWrapping="Wrap" Text="Warning: Historical searches take a longer time to run." VerticalAlignment="Top" Height="56" Width="163" Foreground="#FFF1EC5C"/>
-    </Grid>
+        Title="USAJobs Searcher" SizeToContent="WidthAndHeight" MinWidth="550" Background="#FF313338">
+
+    <Window.Resources>
+        <BooleanToVisibilityConverter x:Key="BoolToVis"/>
+    </Window.Resources>
+
+    <ScrollViewer VerticalScrollBarVisibility="Auto">
+        <StackPanel Margin="20">
+            <TextBlock Text="USAJobs Searcher" 
+                       HorizontalAlignment="Center" FontSize="28" FontFamily="Cascadia Mono"
+                       Foreground="#FFF2F3F5" Margin="0,0,0,20"/>
+
+            <GroupBox Header="Search Criteria" FontSize="16" Foreground="#FFF2F3E6"
+                      Background="#FF313338" Margin="0,0,0,20">
+                <StackPanel Margin="10">
+                    <Grid Margin="0,0,0,10">
+                        <Grid.RowDefinitions>
+                            <RowDefinition Height="Auto"/>
+                            <RowDefinition Height="Auto"/>
+                            <RowDefinition Height="Auto"/>
+                            <RowDefinition Height="Auto"/>
+                        </Grid.RowDefinitions>
+                        <Grid.ColumnDefinitions>
+                            <ColumnDefinition Width="Auto"/>
+                            <ColumnDefinition Width="*"/>
+                        </Grid.ColumnDefinitions>
+
+                        <Label Grid.Row="0" Grid.Column="0" Content="Location:" Width="100" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                        <Grid Grid.Row="0" Grid.Column="1" Margin="0,5">
+                            <TextBox x:Name="LocationTextBox" Height="25" FontSize="14" Background="#FFF2F3E9"/>
+                            <TextBlock x:Name="LocationHint" Text="Ex: Japan, California, Europe" 
+                                       IsHitTestVisible="False" VerticalAlignment="Center" Margin="5,0,0,0"
+                                       Foreground="DarkGray" HorizontalAlignment="Left">
+                                <TextBlock.Style>
+                                    <Style TargetType="TextBlock">
+                                        <Setter Property="Visibility" Value="Collapsed"/>
+                                        <Style.Triggers>
+                                            <DataTrigger Binding="{Binding Text, ElementName=LocationTextBox}" Value="">
+                                                <Setter Property="Visibility" Value="Visible"/>
+                                            </DataTrigger>
+                                        </Style.Triggers>
+                                    </Style>
+                                </TextBlock.Style>
+                            </TextBlock>
+                        </Grid>
+
+                        <Label Grid.Row="1" Grid.Column="0" Content="Job Series:" Width="100" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                        <Grid Grid.Row="1" Grid.Column="1" Margin="0,5">
+                            <TextBox x:Name="JobSeriesTextBox" Height="25" FontSize="14" Background="#FFF2F3E9"/>
+                            <TextBlock x:Name="JobSeriesHint" Text="Ex: 2210" 
+                                       IsHitTestVisible="False" VerticalAlignment="Center" Margin="5,0,0,0"
+                                       Foreground="DarkGray" HorizontalAlignment="Left">
+                                <TextBlock.Style>
+                                    <Style TargetType="TextBlock">
+                                        <Setter Property="Visibility" Value="Collapsed"/>
+                                        <Style.Triggers>
+                                            <DataTrigger Binding="{Binding Text, ElementName=JobSeriesTextBox}" Value="">
+                                                <Setter Property="Visibility" Value="Visible"/>
+                                            </DataTrigger>
+                                        </Style.Triggers>
+                                    </Style>
+                                </TextBlock.Style>
+                            </TextBlock>
+                        </Grid>
+
+                        <Label Grid.Row="2" Grid.Column="0" Content="Remote?" Width="100" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                        <CheckBox x:Name="RemoteJobCheckBox" Grid.Row="2" Grid.Column="1" VerticalAlignment="Center" Margin="0,5"/>
+
+                        <Label Grid.Row="3" Grid.Column="0" Content="Historical?" Width="100" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                        <CheckBox x:Name="HistoricalCheckBox" Grid.Row="3" Grid.Column="1" VerticalAlignment="Center" Margin="0,5"/>
+                    </Grid>
+
+                    <StackPanel x:Name="DatePickersPanel"
+            Visibility="{Binding IsChecked, ElementName=HistoricalCheckBox, Converter={StaticResource BoolToVis}}">
+                        <Grid Margin="0,0,0,5">
+                            <Grid.ColumnDefinitions>
+                                <ColumnDefinition Width="Auto"/>
+                                <ColumnDefinition Width="*"/>
+                            </Grid.ColumnDefinitions>
+                            <Label Grid.Column="0" Content="From Date:" Width="100" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                            <DatePicker x:Name="FromDatePicker" Focusable="False" Grid.Column="1" Margin="0,5,214,5"/>
+                        </Grid>
+                        <Grid>
+                            <Grid.ColumnDefinitions>
+                                <ColumnDefinition Width="Auto"/>
+                                <ColumnDefinition Width="*"/>
+                            </Grid.ColumnDefinitions>
+                            <Label Grid.Column="0" Content="To Date:" Width="100" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                            <DatePicker x:Name="ToDatePicker" Focusable="False" Grid.Column="1" Margin="0,5,214,5"/>
+                        </Grid>
+                    </StackPanel>
+
+                </StackPanel>
+            </GroupBox>
+
+            <GroupBox Header="API Credentials" FontSize="16" Foreground="#FFF2F3E6"
+                      Background="#FF313338" Margin="0,0,0,20">
+                <Grid Margin="10">
+                    <Grid.RowDefinitions>
+                        <RowDefinition Height="Auto"/>
+                        <RowDefinition Height="Auto"/>
+                    </Grid.RowDefinitions>
+                    <Grid.ColumnDefinitions>
+                        <ColumnDefinition Width="Auto"/>
+                        <ColumnDefinition Width="*"/>
+                    </Grid.ColumnDefinitions>
+
+                    <Label Grid.Row="0" Grid.Column="0" Content="API Email Address:" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                    <TextBox x:Name="EmailAddressTextBox" Grid.Row="0" Grid.Column="1" Margin="5" Height="25" FontSize="14" Background="#FFF2F3E9"/>
+
+                    <Label Grid.Row="1" Grid.Column="0" Content="API Key:" Foreground="#FFF2F3F5" VerticalAlignment="Center" Margin="0,5"/>
+                    <TextBox x:Name="APIKeyTextBox" Grid.Row="1" Grid.Column="1" Margin="5" Height="25" FontSize="14" Background="#FFF2F3E9"/>
+                </Grid>
+            </GroupBox>
+
+            <StackPanel Orientation="Vertical" HorizontalAlignment="Center" Margin="0,10">
+                <Button x:Name="SearchButton" Content="Search" Width="90" Height="38" Margin="0,0,0,10"/>
+                <TextBlock x:Name="HistoricalWarningTextBlock"
+                           Text="Warning: Historical searches may take a longer time to run."
+                           Foreground="#FFF1EC5C" TextWrapping="Wrap" TextAlignment="Center"
+                           Width="250"
+                           Visibility="{Binding IsChecked, ElementName=HistoricalCheckBox, Converter={StaticResource BoolToVis}}"/>
+            </StackPanel>
+        </StackPanel>
+    </ScrollViewer>
 </Window>
+
 "@
 
 ## Sanitize/prep the XAML and read it
@@ -74,31 +152,6 @@ $Form = [Windows.Markup.XamlReader]::Load( $reader )
 $XAML.SelectNodes("//*[@Name]") | % {
     Set-Variable -Name "GUI$($_.Name)" -Value $Form.FindName($_.Name) -ErrorAction Stop -Scope Global
 }
-
-## Hide the date pickers by default
-$GUIFromDateLabel.Visibility = "Hidden"
-$GUIFromDatePicker.Visibility = "Hidden"
-$GUIToDateLabel.Visibility = "Hidden"
-$GUIToDatePicker.Visibility = "Hidden"
-$GUIHistoricalWarningTextBlock.Visibility = "Hidden"
-
-## Historical checkbox has been checked, show the date picker options
-$GUIHistoricalCheckBox.Add_Checked({
-    $GUIFromDateLabel.Visibility = "Visible"
-    $GUIFromDatePicker.Visibility = "Visible"
-    $GUIToDateLabel.Visibility = "Visible"
-    $GUIToDatePicker.Visibility = "Visible"
-    $GUIHistoricalWarningTextBlock.Visibility = "Visible"
-})
-
-## Historical checkbox has been unchecked, hide the date picker options
-$GUIHistoricalCheckBox.Add_UnChecked({
-    $GUIFromDateLabel.Visibility = "Hidden"
-    $GUIFromDatePicker.Visibility = "Hidden"
-    $GUIToDateLabel.Visibility = "Hidden"
-    $GUIToDatePicker.Visibility = "Hidden"
-    $GUIHistoricalWarningTextBlock.Visibility = "Hidden"
-})
 
 ## Search button has been clicked, begin the search
 $GUISearchButton.Add_Click({
@@ -119,7 +172,7 @@ $Headers = @{
     'User-Agent' = $Email
     'Authorization-Key' = $APIKey
 }
-$Request = Invoke-RestMethod "https://data.usajobs.gov/api/search?JobCategoryCode=$JobSeries&LocationName=$Location&RemoteIndicator=$Remote" -Headers $Headers
+$Request = Invoke-RestMethod "$($USAJobsSearchAPI)JobCategoryCode=$JobSeries&LocationName=$Location&RemoteIndicator=$Remote" -Headers $Headers
 $ResultCount = $Request.SearchResult.SearchResultCount
 
 $Results = for ($i=0; $i -lt $ResultCount; $i++) {     
@@ -163,18 +216,19 @@ $Output | Out-GridView }
 Elseif ($GUIHistoricalCheckBox.IsChecked -eq $True) {
 
 ## Grab the information required to build the search request
-$APIKey = $GUIAPIKeyTextBox.Text
-$Email = $GUIEmailAddressTextBox.Text
 $Location = $GUILocationTextBox.Text
 $JobSeries = $GUIJobSeriesTextBox.Text
 $Remote = $GUIRemoteJobCheckBox.IsChecked
 $FromDate = ($GUIFromDatePicker.SelectedDate).ToString("MM-dd-yyyy")
 $ToDate = ($GUIToDatePicker.SelectedDate).ToString("MM-dd-yyyy")
 
-## Submit the API Request
-$InitialRequest = Invoke-RestMethod "$($USAJobsHistoricSearchAPI)PositionSeries=$JobSeries&StartPositionOpenDate=$FromDate&EndPositionOpenDate=$ToDate"
+## Submit the API request with supplied parameters, such as the date range
+$InitialRequest = Invoke-RestMethod "$($USAJobsHistoricSearchAPI)PositionSeries=$JobSeries&LocationName=$Location&PositionOpenDate=$FromDate&EndPositionOpenDate=$ToDate"
+
+## Create an empty array to store the results in
 [System.Collections.ArrayList]$OutputArray = @{}
 
+## Get total amount of pages returned from request, submit new request for each if required
 $TotalPages = $InitialRequest.Paging.metadata.totalPages
     for ($i = 1; $i -le $TotalPages; $i++) {
         $RequestURL = "https://data.usajobs.gov/api/historicjoa?PositionSeries=2210&StartPositionOpenDate=01-01-2022&EndPositionOpenDate=03-29-2023&pagesize=1000&pagenumber=$i"
@@ -183,6 +237,7 @@ $TotalPages = $InitialRequest.Paging.metadata.totalPages
             $Output = $Request.data | Where { ($_.PositionLocations -match $Location) -and ($_.TeleworkEligible -eq 'Y') } }
         Elseif ($Remote -eq $False) {
             $Output = $Request.Data | Where { ($_.PositionLocations -match $Location) -and ($_.TeleworkEligible -eq 'N') } }
+
 $Output | % {
     $Object = [PSCustomObject]@{
         'Country' = $_.positionLocations.PositionLocationCountry
